6c44dda0d53b17db4abb84457d59b6093dee18df
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2024-09-18 22:28:27 +0800

    rename & add test for has()

--- state/eth2btcstate/statedb_test.go
+++ state/eth2btcstate/statedb_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestInsertAfterRequested(t *testing.T) {
-	db, err := NewStateDB("sqlite3", ":memory:")
+	db, err := newStateDB("sqlite3", ":memory:")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -18,9 +18,9 @@ func TestInsertAfterRequested(t *testing.T) {
 
 	// Insert a redeem with status == requested
 	r0 := randRedeem(RedeemStatusRequested)
-	err = db.InsertAfterRequested(r0)
+	err = db.insertAfterRequested(r0)
 	assert.NoError(t, err)
-	rs, err := db.GetByStatus(RedeemStatusRequested)
+	rs, err := db.getByStatus(RedeemStatusRequested)
 	assert.Equal(t, 1, len(rs))
 	r1 := rs[0]
 	assert.NoError(t, err)
@@ -35,25 +35,25 @@ func TestInsertAfterRequested(t *testing.T) {
 
 	// Can only insert when status == requested or invalid
 	r0.Status = RedeemStatusPrepared
-	err = db.InsertAfterRequested(r0)
+	err = db.insertAfterRequested(r0)
 	assert.Equal(t, err, stateDBErrors.CannotInsertDueToInvalidStatus(r0))
 
 	// Cannot insert two redeems with the same request tx hash
 	r2 := randRedeem(RedeemStatusRequested)
 	r2.Outpoints = nil
 	r2.RequestTxHash = r0.RequestTxHash
-	err = db.InsertAfterRequested(r2)
+	err = db.insertAfterRequested(r2)
 	assert.NoError(t, err)
-	rs, err = db.GetByStatus(RedeemStatusRequested)
+	rs, err = db.getByStatus(RedeemStatusRequested)
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(rs))
 	assert.Equal(t, rs[0], r1)
 
 	// Insert another redeem
 	r2.RequestTxHash = common.RandBytes32()
-	err = db.InsertAfterRequested(r2)
+	err = db.insertAfterRequested(r2)
 	assert.NoError(t, err)
-	rs, err = db.GetByStatus(RedeemStatusRequested)
+	rs, err = db.getByStatus(RedeemStatusRequested)
 	assert.NoError(t, err)
 	assert.Equal(t, 2, len(rs))
 	assert.Equal(t, rs[0].RequestTxHash, r0.RequestTxHash)
@@ -61,7 +61,7 @@ func TestInsertAfterRequested(t *testing.T) {
 }
 
 func TestUpdateAfterPrepared(t *testing.T) {
-	db, err := NewStateDB("sqlite3", ":memory:")
+	db, err := newStateDB("sqlite3", ":memory:")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -69,31 +69,52 @@ func TestUpdateAfterPrepared(t *testing.T) {
 
 	// Check errors
 	r0 := randRedeem(RedeemStatusRequested)
-	err = db.UpdateAfterPrepared(r0)
+	err = db.updateAfterPrepared(r0)
 	assert.Equal(t, err, stateDBErrors.CannotUpdateDueToInvalidStatus(r0))
 
 	// Insert a redeem with status == requested
 	r1 := randRedeem(RedeemStatusRequested)
 	r1.Outpoints = nil
-	err = db.InsertAfterRequested(r1)
+	err = db.insertAfterRequested(r1)
 	assert.NoError(t, err)
 
 	// update
 	r1.Status = RedeemStatusPrepared
 	r1.Outpoints = []Outpoint{{TxId: common.RandBytes32(), Idx: 0}}
-	err = db.UpdateAfterPrepared(r1)
+	err = db.updateAfterPrepared(r1)
 	assert.NoError(t, err)
 
 	// check
-	rs, err := db.GetByStatus(RedeemStatusPrepared)
+	rs, err := db.getByStatus(RedeemStatusPrepared)
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(rs))
 	r2 := rs[0]
 	assert.Equal(t, r2.BtcTxId, [32]byte{})
 }
 
+func TestHas(t *testing.T) {
+	db, err := newStateDB("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	r := randRedeem(RedeemStatusRequested)
+
+	ok, _, err := db.has(r.RequestTxHash[:])
+	assert.NoError(t, err)
+	assert.False(t, ok)
+
+	err = db.insertAfterRequested(r)
+	assert.NoError(t, err)
+	ok, status, err := db.has(r.RequestTxHash[:])
+	assert.NoError(t, err)
+	assert.True(t, ok)
+	assert.Equal(t, RedeemStatusRequested, status)
+}
+
 func TestKV(t *testing.T) {
-	db, err := NewStateDB("sqlite3", ":memory:")
+	db, err := newStateDB("sqlite3", ":memory:")
 	if err != nil {
 		t.Fatal(err)
 	}
