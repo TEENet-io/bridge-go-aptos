f9bbf58464f1e62f52ddfa21312e9bd21376b264
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2024-09-21 11:44:23 +0800

    ethtxmanagerdb

---
+++ ethtxmanager/ethtxmanagerdb_test.go
@@ -0,0 +1,81 @@
+package ethtxmanager
+
+import (
+	"database/sql"
+	"math/big"
+	"testing"
+
+	"github.com/TEENet-io/bridge-go/common"
+	"github.com/stretchr/testify/assert"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func TestNewEthTxManagerDB(t *testing.T) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	assert.NoError(t, err)
+	defer db.Close()
+
+	etm, err := NewEthTxManagerDB(db)
+	assert.NoError(t, err)
+	defer etm.Close()
+}
+
+func TestSigReqOps(t *testing.T) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	assert.NoError(t, err)
+	defer db.Close()
+
+	etm, err := NewEthTxManagerDB(db)
+	assert.NoError(t, err)
+	defer etm.Close()
+
+	sr := &SignatureRequest{
+		RequestTxHash: common.RandBytes32(),
+		SigningHash:   common.RandBytes32(),
+		Rx:            big.NewInt(100),
+		S:             big.NewInt(200),
+	}
+
+	err = etm.insertSignatureRequest(sr)
+	assert.NoError(t, err)
+
+	sr2, err := etm.GetSignatureRequestByRequestTxHash(sr.RequestTxHash)
+	assert.NoError(t, err)
+	assert.Equal(t, sr, sr2)
+}
+
+func TestMonitoredTxOps(t *testing.T) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	assert.NoError(t, err)
+	defer db.Close()
+
+	etm, err := NewEthTxManagerDB(db)
+	assert.NoError(t, err)
+	defer etm.Close()
+
+	mt := &monitoredTx{
+		TxHash:        common.RandBytes32(),
+		RequestTxHash: common.RandBytes32(),
+		SentAt:        common.RandBytes32(),
+		MinedAt:       common.RandBytes32(),
+	}
+
+	err = etm.insertMonitoredTx(mt)
+	assert.NoError(t, err)
+	mt1, err := etm.GetMonitoredTxByRequestTxHash(mt.RequestTxHash)
+	assert.NoError(t, err)
+	assert.Equal(t, mt1.MinedAt, [32]byte{})
+	mt1.MinedAt = mt.MinedAt
+	assert.Equal(t, mt, mt1)
+
+	err = etm.updateMonitoredTxAfterMined(mt)
+	assert.NoError(t, err)
+	mt2, err := etm.GetMonitoredTxByRequestTxHash(mt.RequestTxHash)
+	assert.NoError(t, err)
+	assert.Equal(t, mt, mt2)
+
+	mt.MinedAt = [32]byte{}
+	err = etm.updateMonitoredTxAfterMined(mt)
+	assert.Equal(t, ErrMintedAtNotSet, err)
+}
