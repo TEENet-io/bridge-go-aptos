bfe758ba51d1ed31b4cf754a952bf1283e13a8a7
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2024-09-23 16:32:43 +0800

    add code for monitoring pending txs

--- ethtxmanager/config.go
+++ ethtxmanager/config.go
@@ -3,11 +3,15 @@ package ethtxmanager
 import "time"
 
 type Config struct {
-	// frequency to get all the requested redeems that have not been prepared
+	// Frequency to get all the requested redeems that have not been prepared
 	FrequencyToGetUnpreparedRedeem time.Duration
 
-	// Timeout on the go routine that waits for a schnorr threshold signature
+	// Timeout on waiting for a schnorr threshold signature
 	TimeoutOnWaitingForSignature time.Duration
 
+	// Timeout on waiting for the spendable outpoints
 	TimeoutOnWaitingForOutpoints time.Duration
+
+	// Frequency to monitor pending transactions
+	FrequencyToMonitorPendingTxs time.Duration
 }
