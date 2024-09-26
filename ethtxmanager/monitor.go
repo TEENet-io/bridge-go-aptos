7e0461cf529719556fb00ebd678264fae24f7732
zzGHzz <40787792+zzGHzz@users.noreply.github.com>
2024-09-26 18:56:16 +0800

    only change status of txs in db insteading of removing them

--- ethtxmanager/monitor.go
+++ ethtxmanager/monitor.go
@@ -5,38 +5,27 @@ import (
 	"errors"
 
 	logger "github.com/0xPolygonHermez/zkevm-node/log"
-	ethcommon "github.com/ethereum/go-ethereum/common"
 )
 
 var (
-	ErrDBOpRemoveMonitoredTx      = errors.New("failed to remove monitored tx after mined")
-	ErrEthermanTransactionReceipt = errors.New("failed to get transaction receipt")
-	ErrEthermanHeaderByNumber     = errors.New("failed to get latest block header by number")
-	ErrEthermanHeaderByHash       = errors.New("failed to get latest block header by hash")
+	ErrDBOpRemoveMonitoredTx       = errors.New("failed to remove monitored tx after mined")
+	ErrDBOpUpdateMonitoredTxStatus = errors.New("failed to update monitored tx status")
+	ErrEthermanTransactionReceipt  = errors.New("failed to get transaction receipt")
+	ErrEthermanHeaderByNumber      = errors.New("failed to get latest block header by number")
+	ErrEthermanHeaderByHash        = errors.New("failed to get latest block header by hash")
 
 	ErrMsgNotFound = "not found"
 )
 
 // monitor monitors the tx until it is mined or timeout
 // Monitoring procedure:
-// 1. Check if the tx is mined, if mined, remove it from db and return
-// 2. Check if the sentAfter block is on canonical chain, if yes, remove it from db and return
-// 3. Check if the tx is timeout for monitoring, if yes, remove it from db and return
-func (txmgr *EthTxManager) monitor(ctx context.Context, mtx *monitoredTx) error {
+// 1. Check if the tx is mined, if mined, update its status to either "success" or "reverted"
+// 2. Check if the tx is timeout for monitoring, if yes, update its status to "timeout"
+func (txmgr *EthTxManager) monitorPendingTxs(ctx context.Context, mtx *MonitoredTx) error {
 	newLogger := logger.WithFields(
 		"txHash", mtx.TxHash.String(),
 		"Id", mtx.Id.String(),
 	)
-
-	removeMonitoredTx := func(txHash ethcommon.Hash) error {
-		err := txmgr.mgrdb.RemoveMonitoredTx(txHash)
-		if err != nil {
-			newLogger.Error("failed to remove monitored tx after mined: err=%v", err)
-			return ErrDBOpRemoveMonitoredTx
-		}
-		newLogger.Debug("removed monitored tx after mined")
-		return nil
-	}
 
 	// get transaction receipt
 	receipt, err := txmgr.etherman.Client().TransactionReceipt(ctx, mtx.TxHash)
@@ -48,7 +37,20 @@ func (txmgr *EthTxManager) monitor(ctx context.Context, mtx *monitoredTx) error 
 	// if the tx is mined, remove it from db
 	if receipt != nil && receipt.BlockNumber != nil {
 		newLogger.Debug("tx has been mined")
-		return removeMonitoredTx(mtx.TxHash)
+
+		var status MonitoredTxStatus
+		if receipt.Status == 0 {
+			newLogger.Error("tx mined but reverted")
+			status = Reverted
+		} else {
+			newLogger.Info("tx mined and successfully executed")
+			status = Success
+		}
+		err := txmgr.mgrdb.UpdateMonitoredTxStatus(mtx.TxHash, status)
+		if err != nil {
+			newLogger.Errorf("failed to update monitored tx status: err=%v", err)
+			return ErrDBOpUpdateMonitoredTxStatus
+		}
 	}
 
 	// check timeout for monitoring the tx
@@ -66,7 +68,12 @@ func (txmgr *EthTxManager) monitor(ctx context.Context, mtx *monitoredTx) error 
 	diff := latest.Number.Uint64() - sentAfter.Number.Uint64()
 	if diff > txmgr.cfg.TimeoutOnMonitoringPendingTxs {
 		newLogger.Errorf("tx has not been mined for %d blocks", txmgr.cfg.TimeoutOnMonitoringPendingTxs)
-		return removeMonitoredTx(mtx.TxHash)
+		err := txmgr.mgrdb.UpdateMonitoredTxStatus(mtx.TxHash, Timeout)
+		if err != nil {
+			newLogger.Errorf("failed to update monitored tx status: err=%v", err)
+			return ErrDBOpUpdateMonitoredTxStatus
+		}
+		return nil
 	}
 
 	return nil
