6355303e5338b8efd72e7d534dae89dd4af9cc61
tofu <tofu@teenet.io>
2024-10-07 17:31:48 +0800

    refine btc vault to use unique identifier for table name

--- btcvault/sqlite_db.go
+++ btcvault/sqlite_db.go
@@ -2,24 +2,26 @@ package btcvault
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3" // SQLite driver
 )
 
 // SQLiteStorage implements VaultUTXOStorage for SQLite
 type SQLiteStorage struct {
-	db *sql.DB
+	uniqueTableID string
+	db            *sql.DB
 }
 
 // NewSQLiteStorage creates a new SQLiteStorage
 // dbFilePath is the path to the SQLite database file
-func NewSQLiteStorage(dbFilePath string) (*SQLiteStorage, error) {
+func NewSQLiteStorage(dbFilePath string, uniqueID string) (*SQLiteStorage, error) {
 	db, err := sql.Open("sqlite3", dbFilePath)
 	if err != nil {
 		return nil, err
 	}
 
-	storage := &SQLiteStorage{db: db}
+	storage := &SQLiteStorage{db: db, uniqueTableID: "vault_utxo_" + uniqueID}
 	if err := storage.init(); err != nil {
 		return nil, err
 	}
@@ -30,41 +32,41 @@ func NewSQLiteStorage(dbFilePath string) (*SQLiteStorage, error) {
 // init initializes the VaultUTXO table and creates an index on tx_id
 // if not existed before.
 func (s *SQLiteStorage) init() error {
-	query := `
-    CREATE TABLE IF NOT EXISTS vault_utxo (
-        block_number INTEGER,
-        block_hash TEXT,
-        tx_id TEXT,
-        vout INTEGER,
-        amount INTEGER,
+	query := fmt.Sprintf(`
+	CREATE TABLE IF NOT EXISTS %s (
+		block_number INTEGER,
+		block_hash TEXT,
+		tx_id TEXT,
+		vout INTEGER,
+		amount INTEGER,
 		lockup BOOLEAN,
-        spent BOOLEAN,
-        timeout INTEGER,
-        PRIMARY KEY (tx_id, vout)
-    );
-    CREATE INDEX IF NOT EXISTS idx_tx_id ON vault_utxo (tx_id);
-    `
+		spent BOOLEAN,
+		timeout INTEGER,
+		PRIMARY KEY (tx_id, vout)
+	);
+	CREATE INDEX IF NOT EXISTS idx_tx_id ON %s (tx_id);
+	`, s.uniqueTableID, s.uniqueTableID)
 	_, err := s.db.Exec(query)
 	return err
 }
 
 // InsertVaultUTXO inserts a new VaultUTXO into the database
 func (s *SQLiteStorage) InsertVaultUTXO(utxo VaultUTXO) error {
-	query := `
-    INSERT INTO vault_utxo (block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?);
-    `
+	query := fmt.Sprintf(`
+	INSERT INTO %s (block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout)
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+	`, s.uniqueTableID)
 	_, err := s.db.Exec(query, utxo.BlockNumber, utxo.BlockHash, utxo.TxID, utxo.Vout, utxo.Amount, utxo.Lockup, utxo.Spent, utxo.Timeout)
 	return err
 }
 
 // QueryByBlockNumber retrieves all VaultUTXOs with the specified block number
 func (s *SQLiteStorage) QueryByBlockNumber(blockNumber int32) ([]VaultUTXO, error) {
-	query := `
-    SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
-    FROM vault_utxo
-    WHERE block_number = ?;
-    `
+	query := fmt.Sprintf(`
+	SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
+	FROM %s
+	WHERE block_number = ?;
+	`, s.uniqueTableID)
 	rows, err := s.db.Query(query, blockNumber)
 	if err != nil {
 		return nil, err
@@ -84,11 +86,11 @@ func (s *SQLiteStorage) QueryByBlockNumber(blockNumber int32) ([]VaultUTXO, erro
 
 // QueryByBlockHash retrieves all VaultUTXOs with the specified block hash
 func (s *SQLiteStorage) QueryByBlockHash(blockHash string) ([]VaultUTXO, error) {
-	query := `
-    SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
-    FROM vault_utxo
-    WHERE block_hash = ?;
-    `
+	query := fmt.Sprintf(`
+	SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
+	FROM %s
+	WHERE block_hash = ?;
+	`, s.uniqueTableID)
 	rows, err := s.db.Query(query, blockHash)
 	if err != nil {
 		return nil, err
@@ -108,11 +110,11 @@ func (s *SQLiteStorage) QueryByBlockHash(blockHash string) ([]VaultUTXO, error) 
 
 // QueryByTxID retrieves all VaultUTXOs with the specified transaction ID
 func (s *SQLiteStorage) QueryByTxID(txID string) ([]VaultUTXO, error) {
-	query := `
-    SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
-    FROM vault_utxo
-    WHERE tx_id = ?;
-    `
+	query := fmt.Sprintf(`
+	SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
+	FROM %s
+	WHERE tx_id = ?;
+	`, s.uniqueTableID)
 	rows, err := s.db.Query(query, txID)
 	if err != nil {
 		return nil, err
@@ -132,11 +134,11 @@ func (s *SQLiteStorage) QueryByTxID(txID string) ([]VaultUTXO, error) {
 
 // QueryByTxIDAndVout retrieves a VaultUTXO with the specified transaction ID and vout
 func (s *SQLiteStorage) QueryByTxIDAndVout(txID string, vout int32) (*VaultUTXO, error) {
-	query := `
-    SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
-    FROM vault_utxo
-    WHERE tx_id = ? AND vout = ?;
-    `
+	query := fmt.Sprintf(`
+	SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
+	FROM %s
+	WHERE tx_id = ? AND vout = ?;
+	`, s.uniqueTableID)
 	var utxo VaultUTXO
 	err := s.db.QueryRow(query, txID, vout).Scan(
 		&utxo.BlockNumber,
@@ -160,11 +162,11 @@ func (s *SQLiteStorage) QueryByTxIDAndVout(txID string, vout int32) (*VaultUTXO,
 // t is the unix timepoint in seconds.
 // all UTXOs with timeout < t are considered as expired.
 func (s *SQLiteStorage) QueryExpiredAndLockedUTXOs(t int64) ([]VaultUTXO, error) {
-	query := `
-    SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
-    FROM vault_utxo
-    WHERE lockup = 1 AND timeout < ?;
-    `
+	query := fmt.Sprintf(`
+	SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
+	FROM %s
+	WHERE lockup = 1 AND timeout < ?;
+	`, s.uniqueTableID)
 	rows, err := s.db.Query(query, t)
 	if err != nil {
 		return nil, err
@@ -184,12 +186,12 @@ func (s *SQLiteStorage) QueryExpiredAndLockedUTXOs(t int64) ([]VaultUTXO, error)
 
 // QueryEnoughUTXOs selects enough UTXOs to cover the specified amount
 func (s *SQLiteStorage) QueryEnoughUTXOs(amount int64) ([]VaultUTXO, error) {
-	query := `
-    SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
-    FROM vault_utxo
-    WHERE lockup = 0 AND spent = 0
-    ORDER BY amount DESC;
-    `
+	query := fmt.Sprintf(`
+	SELECT block_number, block_hash, tx_id, vout, amount, lockup, spent, timeout
+	FROM %s
+	WHERE lockup = 0 AND spent = 0
+	ORDER BY amount DESC;
+	`, s.uniqueTableID)
 	rows, err := s.db.Query(query)
 	if err != nil {
 		return nil, err
@@ -217,33 +219,33 @@ func (s *SQLiteStorage) QueryEnoughUTXOs(amount int64) ([]VaultUTXO, error) {
 
 // SetLockup sets the lockup status of a VaultUTXO identified by txID and vout
 func (s *SQLiteStorage) SetLockup(txID string, vout int32, lockup bool) error {
-	query := `
-    UPDATE vault_utxo
-    SET lockup = ?
-    WHERE tx_id = ? AND vout = ?;
-    `
+	query := fmt.Sprintf(`
+	UPDATE %s
+	SET lockup = ?
+	WHERE tx_id = ? AND vout = ?;
+	`, s.uniqueTableID)
 	_, err := s.db.Exec(query, lockup, txID, vout)
 	return err
 }
 
 // SetSpent sets the spent status of a VaultUTXO identified by txID and vout
 func (s *SQLiteStorage) SetSpent(txID string, vout int32, spent bool) error {
-	query := `
-    UPDATE vault_utxo
-    SET spent = ?
-    WHERE tx_id = ? AND vout = ?;
-    `
+	query := fmt.Sprintf(`
+	UPDATE %s
+	SET spent = ?
+	WHERE tx_id = ? AND vout = ?;
+	`, s.uniqueTableID)
 	_, err := s.db.Exec(query, spent, txID, vout)
 	return err
 }
 
 // SetTimeout sets the expiry timepoint of a VaultUTXO identified by txID and vout
 func (s *SQLiteStorage) SetTimeout(txID string, vout int32, timeout int64) error {
-	query := `
-    UPDATE vault_utxo
-    SET timeout = ?
-    WHERE tx_id = ? AND vout = ?;
-    `
+	query := fmt.Sprintf(`
+	UPDATE %s
+	SET timeout = ?
+	WHERE tx_id = ? AND vout = ?;
+	`, s.uniqueTableID)
 	_, err := s.db.Exec(query, timeout, txID, vout)
 	return err
 }
@@ -251,11 +253,11 @@ func (s *SQLiteStorage) SetTimeout(txID string, vout int32, timeout int64) error
 // SumMoney calculates the total amount of all VaultUTXOs
 // Only the unspent & not locked up UTXOs are counted.
 func (s *SQLiteStorage) SumMoney() (int64, error) {
-	query := `
-    SELECT SUM(amount)
-    FROM vault_utxo
+	query := fmt.Sprintf(`
+	SELECT SUM(amount)
+	FROM %s
 	WHERE lockup = 0 AND spent = 0;
-    `
+	`, s.uniqueTableID)
 	var total int64
 	err := s.db.QueryRow(query).Scan(&total)
 	if err != nil {
