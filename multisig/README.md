cf04cdbfb07ead06a5c4a65f926a4d8d9ea9722c
tofu <tofu@teenet.io>
2025-01-14 14:37:57 +0800

    multisig remote wallet integration & folder structure complete

---
+++ multisig/README.md
@@ -0,0 +1,18 @@
+# Multi-Sig Schnorr Wallet
+
+Multi-sign schnorr wallet is a multi-party wallet that can perform like a single private key backed wallet.
+
+# Functionality
+
+- Sign(): Given a message, sign it and return the signature.
+- GetPublicKey(): Return the public key of the wallet. (for signature verification)
+
+# Types
+
+- LocalWallet: A wallet that can sign messages but is localled hosted.
+- RemoteWallet: A wallet that can sign messages but is remotely hosted on server.
+- SimulatedWallet: A wallet that is backed by only a single private key and hosted locally.
+
+# Files
+
+`net.go` - Operator to communicate with remote server. Do `Sign()` and `GetPubKey()` requests.
---
+++ multisig/net_test.go
@@ -0,0 +1,58 @@
+package multisig
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+var testConfig = ConnectorConfig{
+	UserID:        0,
+	Name:          "client0",
+	Cert:          "config/data/client0.crt",
+	Key:           "config/data/client0.key",
+	CaCert:        "config/data/client0-ca.crt",
+	ServerAddress: "20.205.130.99:6001",
+	ServerCACert:  "config/data/node0-ca.crt",
+}
+
+func setup() (*Connector, error) {
+	if _, err := os.Stat(testConfig.Cert); os.IsNotExist(err) {
+		return nil, err
+	}
+	if _, err := os.Stat(testConfig.Key); os.IsNotExist(err) {
+		return nil, err
+	}
+	if _, err := os.Stat(testConfig.CaCert); os.IsNotExist(err) {
+		return nil, err
+	}
+	if _, err := os.Stat(testConfig.ServerCACert); os.IsNotExist(err) {
+		return nil, err
+	}
+	c, err := NewConnector(&testConfig)
+	return c, err
+}
+
+func TestGetPubKey(t *testing.T) {
+	c, err := setup()
+	if err != nil {
+		t.Fatalf("Error setting up connector: %v", err)
+	}
+	result, err := c.GetPubKey()
+	if err != nil {
+		t.Fatalf("Error getting public key: %v", err)
+	}
+	fmt.Printf("Group Public Key: %x\n", result)
+}
+
+func TestGetSignature(t *testing.T) {
+	c, err := setup()
+	if err != nil {
+		t.Fatalf("Error setting up connector: %v", err)
+	}
+	result, err := c.GetSignature([]byte("hello1"))
+	if err != nil {
+		t.Fatalf("Error getting signature: %v", err)
+	}
+	fmt.Printf("Signature: %x\n", result)
+}
