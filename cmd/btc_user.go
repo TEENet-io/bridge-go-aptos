9f32e6f6e452704ee7155c083d7b434754914299
tofu <tofu@teenet.io>
2025-02-16 00:28:24 +0800

    btc user command line tool finished

--- cmd/btc_user.go
+++ cmd/btc_user.go
@@ -5,8 +5,212 @@
 
 package cmd
 
+import (
+	"fmt"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	logger "github.com/sirupsen/logrus"
+
+	"github.com/TEENet-io/bridge-go/btcman/assembler"
+	btcrpc "github.com/TEENet-io/bridge-go/btcman/rpc"
+	"github.com/TEENet-io/bridge-go/btcman/utxo"
+)
+
+const (
+	BTC_MATURE_OFFSET     = 1 // if tx is MATURE_OFFSET blocks old, we consider safe.
+	REGTEST_COINBASE_ADDR = "mkVXZnqaaKt4puQNr4ovPHYg48mjguFCnT"
+	REGTEST_ENOUGH_BLOCKS = 100
+	REGTEST_FEE_SATOSHI   = 0.001 * 1e8 // 0.001 btc
+)
+
 type BtcUserConfig struct {
+	BtcRpcServer   string // btc rpc server info
+	BtcRpcPort     string // btc rpc server info
+	BtcRpcUsername string // btc rpc server info
+	BtcRpcPwd      string // btc rpc server info
+
+	BtcChainConfig *chaincfg.Params // regtest, testnet, mainnet? see btcman/assembler/common.go
+
+	BtcCoreAccountPriv string // user's btc private key.
+	BtcCoreAccountAddr string // user's btc address.
 }
 
 type BtcUser struct {
+	BtcRpcClient   *btcrpc.RpcClient       // rpc client
+	MyLegacySigner *assembler.LegacySigner // user's signer
+	MyUserConfig   *BtcUserConfig          // contains a copy of user's config.
+}
+
+// Create a new BTC user.
+// registerAddress: if true, force register user's address to the bitcoin core rpc node for tracking.
+// you can set it to false if using a public btc node because the address is always tracking (I guess).
+// For private bitcoin node setup you need to set it to true.
+func NewBtcUser(buc *BtcUserConfig, registerAddress bool) (*BtcUser, error) {
+	// Set up BTC PRC connection.
+	// We use that to interact with BTC blockchain.
+	myBtcRpcClient, err := SetupBtcRpc(buc.BtcRpcServer, buc.BtcRpcPort, buc.BtcRpcUsername, buc.BtcRpcPwd)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set up signing entity of user
+	// We use that to unlock funds and sign transactions.
+	_basic_signer, err := assembler.NewBasicSigner(buc.BtcCoreAccountPriv, buc.BtcChainConfig)
+	if err != nil {
+		logger.WithField("user_priv", buc.BtcCoreAccountPriv).Error("cannot create Basic Signer by private key")
+		return nil, err
+	}
+	_legacy_signer, err := assembler.NewLegacySigner(*_basic_signer)
+	if err != nil {
+		logger.Error("cannot create legacy signer from basic signer")
+		return nil, err
+	}
+
+	_user_addr := _legacy_signer.P2PKH.EncodeAddress()
+	logger.WithField("P2PKH_addr", _user_addr).Info("user btc signer")
+
+	if _user_addr != buc.BtcCoreAccountAddr {
+		logger.WithFields(logger.Fields{
+			"declared_addr": buc.BtcCoreAccountAddr,
+			"actual_addr":   _user_addr,
+		}).Error("user btc address mismatch from provided and decoded")
+		return nil, err
+	}
+
+	if registerAddress {
+		// Register user's address to the bitcoin core node.
+		// This is necessary for tracking user's balance and transactions.
+		// If you are using a public node, you can skip this step.
+		err = myBtcRpcClient.ImportAddress(_user_addr, "")
+		if err != nil {
+			logger.WithField("user_addr", _user_addr).Error("cannot import user address to btc rpc node")
+			return nil, err
+		}
+	}
+
+	return &BtcUser{
+		BtcRpcClient:   myBtcRpcClient,
+		MyLegacySigner: _legacy_signer,
+		MyUserConfig:   buc,
+	}, nil
+}
+
+func (bu *BtcUser) Close() {
+	bu.BtcRpcClient.Close() // release rpc connection.
+}
+
+func (bu *BtcUser) GetUtxos() ([]utxo.UTXO, error) {
+	utxos, err := bu.BtcRpcClient.GetUtxoList(bu.MyLegacySigner.P2PKH, BTC_MATURE_OFFSET)
+	if err != nil {
+		logger.WithFields(logger.Fields{
+			"user_address": bu.MyLegacySigner.P2PKH.EncodeAddress(),
+			"error":        err,
+		}).Error("cannot retrieve utxos from rpc")
+		return nil, err
+	}
+	if len(utxos) == 0 {
+		logger.WithField("user_address", bu.MyLegacySigner.P2PKH.EncodeAddress()).Info("no utxos to spend, send some bitcoin to this address first")
+	}
+	logger.WithField("count", len(utxos)).Info("User UTXO(s)")
+	return utxos, nil
+}
+
+func (bu *BtcUser) GetBalance() (int64, error) {
+	balance, err := bu.BtcRpcClient.GetBalance(bu.MyLegacySigner.P2PKH, BTC_MATURE_OFFSET)
+	if err != nil {
+		logger.WithFields(logger.Fields{
+			"user_address": bu.MyLegacySigner.P2PKH.EncodeAddress(),
+			"error":        err,
+		}).Error("cannot retrieve balance from rpc")
+		return 0, err
+	}
+	logger.WithField("balance", balance).Info("User Balance")
+	return balance, nil
+}
+
+// amount: satoshi
+// fee: satoshi
+func (bu *BtcUser) DepositToBridge(amount int64, feeAmount int64, bridgeAddress string, evmAddr string, evmChainId int) (string, error) {
+	logger.WithFields(logger.Fields{
+		"amount":   amount,
+		"evm_addr": evmAddr,
+		"evm_id":   evmChainId,
+	}).Info("Deposit data")
+
+	// safe guard: check if user has enough balance to make a deposit.
+	balance, err := bu.GetBalance()
+	if err != nil {
+		return "", err
+	}
+	if balance < amount+feeAmount {
+		logger.WithFields(logger.Fields{
+			"balance":        balance,
+			"requiredAmount": amount + feeAmount,
+		}).Error("not enough balance to make a deposit")
+		return "", fmt.Errorf("not enough balance: have %d, need %d", balance, amount+feeAmount)
+	}
+
+	// Fetch UTXOs
+	utxos, err := bu.GetUtxos()
+	if err != nil {
+		return "", err
+	}
+
+	// Select barely enough UTXO(s) to spend
+	selected_utxos, err := utxo.SelectUtxo(convertToPointerSlice(utxos), amount, feeAmount)
+	if err != nil {
+		logger.WithField("error", err).Error("cannot select enough utxos for deposit")
+		return "", err
+	}
+	logger.WithField("count", len(selected_utxos)).Info("User UTXOs selected")
+
+	// Craft [Deposit Tx]
+	tx, err := bu.MyLegacySigner.MakeBridgeDepositTx(
+		selected_utxos,
+		bridgeAddress,
+		amount,
+		feeAmount,
+		bu.MyUserConfig.BtcCoreAccountAddr,
+		evmAddr,
+		evmChainId,
+	)
+	if err != nil {
+		logger.WithField("error", err).Error("cannot create Tx")
+		return "", err
+	}
+
+	logger.WithFields(logger.Fields{
+		"TxIn":  len(tx.TxIn),
+		"TxOut": len(tx.TxOut),
+	}).Info("Crafted deposit Tx")
+
+	// Send [Deposit Tx] via RPC
+	depositBtcTxHash, err := bu.BtcRpcClient.SendRawTx(tx)
+	if err != nil {
+		logger.WithField("error", err).Error("send raw Tx error")
+		return "", err
+	}
+
+	logger.WithField("txHash", depositBtcTxHash.String()).Info("Tx sent")
+	return depositBtcTxHash.String(), nil
+}
+
+func (bu *BtcUser) MineEnoughBlocks() ([]*chainhash.Hash, error) {
+	if bu.MyUserConfig.BtcChainConfig != &chaincfg.RegressionNetParams {
+		logger.Error("mine blocks only works in regtest mode")
+		return nil, fmt.Errorf("MineEnoughBlocks() only works in btc regtest mode")
+	}
+
+	_coinbase_addr, _ := assembler.DecodeAddress(REGTEST_COINBASE_ADDR, bu.MyUserConfig.BtcChainConfig)
+	return bu.BtcRpcClient.GenerateBlocks(REGTEST_ENOUGH_BLOCKS, _coinbase_addr)
+}
+
+// Convert from [] to []*
+func convertToPointerSlice(utxos []utxo.UTXO) []*utxo.UTXO {
+	utxoPtrs := make([]*utxo.UTXO, len(utxos))
+	for i := range utxos {
+		utxoPtrs[i] = &utxos[i]
+	}
+	return utxoPtrs
 }
--- cmd/btc_user_cmd/main.go
+++ cmd/btc_user_cmd/main.go
@@ -1,5 +1,232 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/TEENet-io/bridge-go/cmd"
+	"github.com/TEENet-io/bridge-go/logconfig"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/spf13/viper"
+)
+
+const (
+	ENV_CONFIG_FILE_PATH = "BTC_USER_CONFIG"
+)
+
 func main() {
-	// Start the server
+	// Set overall config level to Debug
+	logconfig.ConfigDebugLogger()
+
+	// Tool to read environment variables
+	viper.AutomaticEnv()
+
+	// Accessing an environment variable of configuration file location.
+	_config_file := viper.GetString(ENV_CONFIG_FILE_PATH)
+	fmt.Printf("BTC user configuration file = %s\n", _config_file)
+
+	// See if file exists
+	if !cmd.FileExists(_config_file) {
+		fmt.Printf("BTC user configuration file not found: %s\n", _config_file)
+		return
+	}
+
+	// Read from config file.
+	success := initializeViper(_config_file)
+	if !success {
+		return
+	}
+
+	// Prepre the BTC user configuration
+	buc := PrepareBtcUserConfig()
+	if buc == nil {
+		fmt.Printf("Error prepare BTC user configuration\n")
+		return
+	}
+
+	bu, err := cmd.NewBtcUser(buc, viper.GetBool("REGISTER_USER_ON_CHAIN"))
+	if err != nil {
+		fmt.Printf("Error creating BTC user: %s\n", err)
+		return
+	}
+
+	fmt.Println(strings.Repeat("=", 30))
+	fmt.Println("Welcome to bridge BTC user command line tool.")
+	fmt.Printf("Your BTC address: %s\n", bu.MyUserConfig.BtcCoreAccountAddr)
+
+	// *** user interactive program ***
+
+	// Create a cancelable context and signal handler for graceful shutdown.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Set up signal handler to catch Ctrl-C.
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		_captured := <-sig
+		fmt.Printf("\nReceived interrupt signal, shutting down... %v\n", _captured)
+		cancel()
+		os.Exit(0)
+	}()
+
+	// gather user inputs
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		// Check if context is done (just in case)
+		select {
+		case <-ctx.Done():
+			bu.Close() // release btcUser resources.
+			return
+		default:
+		}
+
+		// Print options
+		fmt.Println("What to do:")
+		fmt.Println("1) View balance")
+		fmt.Println("2) View recent UTXOs")
+		fmt.Println("3) Send deposit")
+		fmt.Println("4) Tell BTC network to mine blocks (regtest only)")
+		fmt.Print("Type option and press Enter: ")
+
+		// Wait for input.
+		if !scanner.Scan() {
+			break
+		}
+		input := strings.TrimSpace(scanner.Text())
+
+		// Process user input.
+		switch input {
+		case "1":
+			_balance, err := bu.GetBalance()
+			if err != nil {
+				fmt.Printf("Error getting balance: %s\n", err)
+			} else {
+				fmt.Printf("Your balance: %d\n", _balance)
+			}
+		case "2":
+			_utxos, err := bu.GetUtxos()
+			if err != nil {
+				fmt.Printf("Error getting UTXOs: %s\n", err)
+			} else {
+				for idx, _utxo := range _utxos {
+					fmt.Printf("[%d]: TxId %s, vout %d, %d satoshi\n", idx, _utxo.TxID, _utxo.Vout, _utxo.Amount)
+				}
+			}
+			// Your action 2 code here.
+		case "3":
+			fmt.Println("Sending a deposit to bridge...")
+			sendDeposit(bu)
+		case "4":
+			fmt.Println("Only use this option in local regtest mode.")
+			fmt.Println("In production / public node it has no effect!")
+			_blks, err := bu.MineEnoughBlocks()
+			if err != nil {
+				fmt.Printf("Error mining blocks: %s\n", err)
+			} else {
+				fmt.Printf("Mined %d blocks\n", len(_blks))
+			}
+		default:
+			fmt.Println("Unknown option, try again.")
+		}
+		fmt.Println()
+	}
+}
+
+func initializeViper(filePath string) bool {
+	viper.SetConfigFile(filePath)
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading configuration file, %s", err)
+		return false
+	}
+	return true
+}
+
+func PrepareBtcUserConfig() *cmd.BtcUserConfig {
+	// Parse the BTC chain config (e.g., "regtest", "testnet", or "mainnet").
+	var btcParams *chaincfg.Params
+	switch viper.GetString("BTC_CHAIN_CONFIG") {
+	case "testnet":
+		btcParams = &chaincfg.TestNet3Params
+	case "mainnet":
+		btcParams = &chaincfg.MainNetParams
+	case "regtest":
+		btcParams = &chaincfg.RegressionNetParams
+	default:
+		// default to regtest
+		btcParams = &chaincfg.RegressionNetParams
+	}
+
+	return &cmd.BtcUserConfig{
+		BtcRpcServer:       viper.GetString("BTC_RPC_SERVER"),
+		BtcRpcPort:         viper.GetString("BTC_RPC_PORT"),
+		BtcRpcUsername:     viper.GetString("BTC_RPC_USERNAME"),
+		BtcRpcPwd:          viper.GetString("BTC_RPC_PWD"),
+		BtcChainConfig:     btcParams,
+		BtcCoreAccountPriv: viper.GetString("BTC_CORE_ACCOUNT_PRIV"),
+		BtcCoreAccountAddr: viper.GetString("BTC_CORE_ACCOUNT_ADDR"),
+	}
+}
+
+func sendDeposit(bu *cmd.BtcUser) (string, error) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Enter deposit address of bridge (BTC): ")
+	scanner.Scan()
+	bridgeBtcWalletAddress := strings.TrimSpace(scanner.Text())
+
+	fmt.Print("Enter amount to send (in satoshis): ")
+	scanner.Scan()
+	amountToSend, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+	if err != nil {
+		fmt.Printf("Invalid amount: %s\n", err)
+		return "", err
+	}
+
+	fmt.Print("Enter Tx fee amount (in satoshis, recommend minimum 100000 for regtest): ")
+	scanner.Scan()
+	feeAmount, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+	if err != nil {
+		fmt.Printf("Invalid fee amount: %s\n", err)
+		return "", err
+	}
+
+	fmt.Print("Enter the eth address of receiver on Ethereum: ")
+	scanner.Scan()
+	ethReceiverAddress := strings.TrimSpace(scanner.Text())
+
+	fmt.Print("Enter Ethereum network ID (number): ")
+	scanner.Scan()
+	ethNetworkID, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+	if err != nil {
+		fmt.Printf("Invalid Ethereum network ID: %s\n", err)
+		return "", err
+	}
+
+	fmt.Printf("Sending %d satoshis to %s with a fee of %d satoshis. Receiver on Ethereum: %s, Network ID: %d\n",
+		amountToSend, bridgeBtcWalletAddress, feeAmount, ethReceiverAddress, ethNetworkID)
+
+	// check if user's balance is enough
+	balance, err := bu.GetBalance()
+	if err != nil {
+		fmt.Printf("Error getting balance: %s\n", err)
+		return "", err
+	}
+	if balance < int64(amountToSend+feeAmount) {
+		fmt.Printf("Not enough balance: have %d, need %d\n", balance, amountToSend+feeAmount)
+	}
+	// Here you would call the function to send the deposit using the collected inputs.
+	btcTxId, err := bu.DepositToBridge(int64(amountToSend), int64(feeAmount), bridgeBtcWalletAddress, ethReceiverAddress, ethNetworkID)
+	if err != nil {
+		fmt.Printf("Error sending deposit: %s\n", err)
+		return "", err
+	}
+	return btcTxId, nil
 }
